132b357269742fe6aa4128eee46faed89e22eaf2
JuanLu23 <39525825+JuanLu23@users.noreply.github.com>
2022-12-14 15:16:48 -0500

    Por el poder de GAYSCOOOOOOOREEEEEEEEEEEEEEEEEEEEE AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Fish-Freedome/Assets/Scripts/Managers/Stage_Selector/Stage_Selector_Manager.cs
+++ Fish-Freedome/Assets/Scripts/Managers/Stage_Selector/Stage_Selector_Manager.cs
@@ -3,42 +3,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stage_Selector_Manager : MonoBehaviour
+public class Stage_Selector_Manager : MonoBehaviour, ISavable
 {
     public Stage_Data[] stageData;
 
     public int totalAmountofCoins;
 
+    public int coinscollected1;
+    public int coinscollected2;
+    public int coinscollected3;
+
+
     private void Start()
     {
         RundDownInfo();
+        coinscollected1 = stageData[0].coinsCollected;
+        coinscollected2 = stageData[1].coinsCollected;
+        coinscollected3 = stageData[2].coinsCollected;
+    }
+
+    public void Update()
+    {
     }
 
     void RundDownInfo()
     {
-        for(int i = 0; i < stageData.Length; i++)
+        for (int i = 0; i < stageData.Length; i++)
         {
             //Debug.Log(stageData[i].coinsCollected);
         }
     }
 
-    /*public object SaveState()
+    public void UpdateDataValues()
+    {
+        stageData[0].coinsCollected = coinscollected1;
+        stageData[1].coinsCollected = coinscollected2;
+        stageData[2].coinsCollected = coinscollected3;
+        Debug.Log("obamium");
+    }
+
+    public object SaveState()
     {
         return new SaveData()
         {
-            stageData = this.stageData
+            coinscollected1 = coinscollected1,
+            coinscollected2 = coinscollected2,
+            coinscollected3 = coinscollected3
         };
     }
+
+
 
     public void LoadState(object state)
     {
         var saveData = (SaveData)state;
-        stageData = saveData.stageData;
+        coinscollected1 = saveData.coinscollected1;
+        coinscollected2 = saveData.coinscollected2;
+        coinscollected3 = saveData.coinscollected3;
+        Debug.Log("obamium");
     }
 
     [Serializable]
     private struct SaveData
     {
-        public Stage_Data[] stageData;
-    }*/
+        public int coinscollected1;
+        public int coinscollected2;
+        public int coinscollected3;
+    }
 }
